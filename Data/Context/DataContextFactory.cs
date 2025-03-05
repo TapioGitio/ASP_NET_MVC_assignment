48ce27c48c4eeb2af63462d4077e47a67630c8f4
Janne <tapioninetytwo@live.se>
2025-03-05 13:15:24 +0100

    Setup the datalayer with the needed folders and datacontext. Have to change the connectionstring later though

---
+++ Data/Context/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Data.Context;
+
+class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        optionsBuilder.UseSqlServer(@"Server=localhost;Database=MyDatabase;User Id=sa;Password=yourStrong(!)Password");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
