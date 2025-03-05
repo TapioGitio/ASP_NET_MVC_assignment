fca52f1f0e00391f9d1c94e229ad386e544fd56a
Janne <tapioninetytwo@live.se>
2025-03-05 12:37:05 +0100

    Cloned to pc and added data and business layer, aswell as erased unwanted files

--- AlphaWebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AlphaWebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
