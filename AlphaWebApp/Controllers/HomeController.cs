eba9e5723eb9db50fb1467f2f7157853d092e053
Janne <tapioninetytwo@live.se>
2025-03-06 18:20:53 +0100

    Added login page

--- AlphaWebApp/Controllers/HomeController.cs
+++ AlphaWebApp/Controllers/HomeController.cs
@@ -8,5 +8,10 @@ namespace AlphaWebApp.Controllers
         {
             return View();
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
