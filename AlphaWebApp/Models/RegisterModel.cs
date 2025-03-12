b943257b4767b8486e200393440b43df5264da81
Janne <tapioninetytwo@live.se>
2025-03-12 09:48:38 +0100

    Changed some prompts

--- AlphaWebApp/Models/RegisterModel.cs
+++ AlphaWebApp/Models/RegisterModel.cs
@@ -4,15 +4,15 @@ namespace AlphaWebApp.Models;
 
 public class RegisterModel
 {
-    [Display(Name = "First Name", Prompt = "Enter your first name")]
+    [Display(Name = "First Name", Prompt = "Your first name")]
     [Required(ErrorMessage = "Required")]
     public string FirstName { get; set; } = null!;
 
-    [Display(Name = "Last Name", Prompt = "Enter your last name")]
+    [Display(Name = "Last Name", Prompt = "Your last name")]
     [Required(ErrorMessage = "Required")]
     public string LastName { get; set; } = null!;
 
-    [Display(Name = "Email", Prompt = "Enter your Email")]
+    [Display(Name = "Email", Prompt = "Your email")]
     [Required(ErrorMessage = "Required")]
     [DataType(DataType.EmailAddress)]
     [RegularExpression(@"^(?!.*\.\.)[a-zA-Z0-9!#$%&'*+/=?^_`{|}~.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,50}$", ErrorMessage = "Invalid Email")]
@@ -24,7 +24,7 @@ public class RegisterModel
     [RegularExpression(@"^(?=.*[a-ö])(?=.*[A-Ö])(?=.*\d)[A-Öa-ö\d]{8,50}$", ErrorMessage = "Invalid format" )]
     public string Password { get; set; } = null!;
 
-    [Display(Name = "Confirm Password", Prompt = "Enter password again..")]
+    [Display(Name = "Confirm Password", Prompt = "Your password again..")]
     [Required(ErrorMessage = "Required")]
     [DataType(DataType.Password)]
     [Compare(nameof(Password), ErrorMessage = "Passwords doesn't match!")]
