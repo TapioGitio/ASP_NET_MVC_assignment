6212e5e6b8b4d3e779764801aec3f6d01490008d
Janne <tapioninetytwo@live.se>
2025-03-20 09:42:51 +0100

    Repos and interfaces created in data layer

---
+++ Data/Repositories/ProjectRepository.cs
@@ -0,0 +1,19 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class ProjectRepository(DataContext context) : BaseRepository<ProjectEntity>(context), IProjectRepository
+{
+    public async Task<ProjectEntity?> ReadOneIncludeAllAsync(int Id)
+    {
+        var result = await _dbSet
+            .Include(x => x.Member)
+            .Include(x => x.Status)
+            .FirstOrDefaultAsync(x => x.Id == Id);
+
+        return result;
+    }
+}
