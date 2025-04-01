d0d616651d0bd1e1653eb10a49452878a0970fbf
Janne <tapioninetytwo@live.se>
2025-04-01 11:06:22 +0200

    Added OAuth button and logic but not the secrets and id nor program. Making this commit incase something goes wrong

--- AlphaWebApp/Controllers/AuthController.cs
+++ AlphaWebApp/Controllers/AuthController.cs
@@ -1,12 +1,17 @@
 ﻿using Business.Interfaces;
+using Data.Entities;
 using Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace AlphaWebApp.Controllers;
 
-public class AuthController(IAuthService authService) : Controller
+public class AuthController(IAuthService authService, SignInManager<MemberEntity> signInManager, UserManager<MemberEntity> userManager) : Controller
 {
     private readonly IAuthService _authService = authService;
+    private readonly SignInManager<MemberEntity> _signInManager = signInManager;
+    private readonly UserManager<MemberEntity> _userManager = userManager;
 
 
     public IActionResult Login()
@@ -28,6 +33,71 @@ public class AuthController(IAuthService authService) : Controller
 
         ViewBag.ErrorMessage = "Invalid email or password";
         return View(form);
+    }
+
+    [HttpPost]
+    public IActionResult ExternalLogin(string provider, string returnUrl = null!)
+    {
+        if (string.IsNullOrEmpty(provider))
+        {
+            ModelState.AddModelError("", "Invalid provider");
+            return View("Login");
+        }
+
+        var redirectUrl = Url.Action("ExternalLoginCallback", "Auth", new { returnUrl })!;
+        var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+        return Challenge(properties, provider);
+    }
+
+    public async Task<IActionResult> ExternalLoginCallback(string returnUrl = null!, string remoteError = null!)
+    {
+        returnUrl ??= Url.Action("Index", "Admin")!;
+
+        if (!string.IsNullOrEmpty(remoteError))
+        {
+            ModelState.AddModelError("", $"Error from external provider: {remoteError}");
+            return View("Login");
+        }
+
+        var loginInfo = await _signInManager.GetExternalLoginInfoAsync();
+        if (loginInfo == null)
+            return RedirectToAction("Login");
+
+        var loginResult = await _signInManager.ExternalLoginSignInAsync(loginInfo.LoginProvider, loginInfo.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+        if (loginResult.Succeeded)
+            return LocalRedirect(returnUrl);
+        else
+        {
+            string firstName = string.Empty;
+            string lastName = string.Empty;
+
+            try
+            {
+                firstName = loginInfo.Principal.FindFirstValue(ClaimTypes.GivenName)!;
+                lastName = loginInfo.Principal.FindFirstValue(ClaimTypes.Surname)!;
+            }
+            catch { }
+
+            string email = loginInfo.Principal.FindFirstValue(ClaimTypes.Email)!;
+            string userName = $"ext_{loginInfo.LoginProvider.ToLower()}_{email}";
+
+            var user = new MemberEntity { UserName = userName, Email = email, FirstName = firstName, LastName = lastName };
+
+            var identityResult = await _userManager.CreateAsync(user);
+            if (identityResult.Succeeded)
+            {
+                await _userManager.AddLoginAsync(user, loginInfo);
+                await _signInManager.SignInAsync(user, isPersistent: false);
+                return LocalRedirect(returnUrl);
+            }
+
+            foreach (var error in identityResult.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View("Login");
+        }
     }
 
     [Route("Register")]
@@ -53,7 +123,7 @@ public class AuthController(IAuthService authService) : Controller
                 return View(form);
             }
         }
-       
+
         return View(form);
     }
 
