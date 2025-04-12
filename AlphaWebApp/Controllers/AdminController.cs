5215b95742f74c82bbfba5eff3a2d3e919f2bb85
Janne <tapioninetytwo@live.se>
2025-04-12 13:35:46 +0200

    Okay back on track, somehow when i deleted uploaded image files. All the controllers were deleted aswell. Took some time to spot this mistake!

---
+++ AlphaWebApp/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlphaWebApp.Controllers;
+
+[Authorize]
+public class AdminController : Controller
+{
+
+    [Route("dashboard")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
